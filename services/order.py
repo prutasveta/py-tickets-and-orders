671b5d479f3211033fdb09b063e6cc140ea834e4
Svitlana <svetapruta@gmail.com>
2025-03-13 12:04:00 +0100

    Solution

---
+++ services/order.py
@@ -0,0 +1,43 @@
+import datetime
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket
+
+
+@transaction.atomic
+def create_order(
+        tickets: list[dict],
+        username: str,
+        date: str = None
+) -> Order:
+
+    user = get_user_model().objects.get(username=username)
+
+    if date is not None:
+        created_at = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M")
+    else:
+        created_at = datetime.datetime.now()
+
+    order = Order.objects.create(
+        user=user,
+        created_at=created_at
+    )
+    order.save()
+
+    for ticket in tickets:
+        Ticket.objects.create(
+            movie_session_id=ticket["movie_session"],
+            order=order,
+            row=ticket["row"],
+            seat=ticket["seat"]
+        )
+
+    return order
+
+
+def get_orders(username: str = None) -> QuerySet[Order]:
+    if username:
+        return Order.objects.filter(user__username=username)
+    return Order.objects.all()
--- settings.py
+++ settings.py
@@ -15,6 +15,26 @@ DATABASES = {
     }
 }
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME":
+            "django.contrib.auth.password_validation."
+            "UserAttributeSimilarityValidator"
+    },
+    {
+        "NAME":
+            "django.contrib.auth.password_validation.MinimumLengthValidator"
+    },
+    {
+        "NAME":
+            "django.contrib.auth.password_validation.CommonPasswordValidator"
+    },
+    {
+        "NAME":
+            "django.contrib.auth.password_validation.NumericPasswordValidator"
+    },
+]
+
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "Europe/Kiev"
@@ -24,5 +44,9 @@ USE_I18N = True
 USE_TZ = False
 
 INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
     "db",
 ]
+
+AUTH_USER_MODEL = "db.User"
